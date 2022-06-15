db2742cfdce61b9aaf1e46db943109ae8613e88a
Chris McNeilly <cmcneilly0108@gmail.com>
2022-06-15 08:32:20 -0700

    will this work

--- LeagueEval/server.R
+++ LeagueEval/server.R
@@ -1,10 +1,14 @@
+library("plotly")
+library("bslib")
 
-setwd("../")
+setwd("../code/")
 source("./inSeasonPulse.r")
-setwd("./LeagueEval")
+#setwd("../LeagueEval")
 
 teams <- sort(unique(RTot$Team))
 
+htrend <- read.csv("./hTrend.csv")
+htrend$date <- mdy(htrend$date)
 
 
 shinyServer(function(input, output,session) {
@@ -17,7 +21,9 @@ shinyServer(function(input, output,session) {
 #  output$TeamH <- renderGvis({ gvisTable(steamH())})
   output$TeamH <- renderDataTable({ pullTeam(input$e1)[[1]] })
   output$TeamP <- renderDataTable({ pullTeam(input$e1)[[2]] })
-
+  output$RTot <- renderDataTable({ RTot })
+  output$problems <- renderDataTable({ problems })
+  
   tprof <- reactive({
     ifelse(input$e2 %in% c('SP','MR','CL'),df<-AllP,df<-AllH)
     f <- df %>% filter(Pos == input$e2,pDFL > input$pd) %>% group_by(Team) %>% summarize(nGood = length(Team))
@@ -38,5 +44,30 @@ shinyServer(function(input, output,session) {
   })
   output$undP <- renderDataTable({up()})
 
+  # Top By Position
+  topPos <- reactive({
+#    f <- df %>% filter(Pos == input$e3,pDFL > input$pd) %>% group_by(Team) %>% summarize(nGood = length(Team))
+#    f2 <- df %>% filter(Pos == input$e3) %>% group_by(Team) %>% summarize(nTotal = length(Team))
+#    ff <- left_join(f2,f,by=c('Team')) %>% arrange(-nGood,-nTotal)
+    ifelse(input$e3 %in% c('SP','MR','CL'),
+           ff <- AllP %>% filter(Pos == input$e3) %>% arrange(-pDFL) %>% 
+             select(Player,Pos,pDFL,Team,Salary,Contract,pSGP,Rank,pW,pSO,pSV,pHLD,pERA,pK.9,pFIP,W,K,S,HD,ERA,hotscore,LVG,Injury,Expected.Return),
+           ff <- AllH %>% filter(str_detect(posEl,input$e3)) %>%
+             select(Player,Pos,Age,pDFL,Team,pSGP,Rank,pHR,pRBI,pR,pSB,pAVG,HR,RBI,R,SB,AVG,hotscore,Injury,Expected.Return) %>%
+             arrange(-pDFL)
+    )
+    ff
+  })
+  output$topPlayers <- renderDataTable({topPos()})
 
+  output$lcgraph <- renderPlotly({
+    plot_ly(htrend, x = ~date, y = ~hotscore)  %>%
+      filter(Player %in% input$choice) %>%
+      group_by(Player) %>%
+      add_lines(color=~Player)
+  })
+  output$g1 <- renderPlot(print(g1),res=120)
+  output$g2 <- renderPlot(print(g2),res=120)
+  output$g3 <- renderPlot(print(g3),res=120)
+  
 })
--- LeagueEval/ui.R
+++ LeagueEval/ui.R
@@ -1,64 +1,116 @@
 # 1st Tool - pullTeam
 
-shinyUI(navbarPage("League Evaluator, v0.1",
-  tabPanel("by Team",
-    sidebarLayout(fluid=FALSE,
-      sidebarPanel(
-        selectizeInput(
-          'e1', 'Select Team', choices=NULL)
-      ),
-      mainPanel(
-        h2(textOutput("tname")),
-        tabsetPanel(type='tabs',
-#          tabPanel('Hitters',htmlOutput("TeamH")),
-          tabPanel('Hitters',dataTableOutput("TeamH")),          
-          tabPanel('Pitchers',dataTableOutput("TeamP"))
+shinyUI(
+  navbarPage(
+    theme = bs_theme(version = 4, bootswatch = "slate"),
+    "DAFL Evaluator, v1.1",
+    tabPanel("Talent View",
+             mainPanel(
+               h2("Rest of Season Prediction"),
+               dataTableOutput("RTot")
+             )),
+    tabPanel(
+      "Hotness by Team",
+      sidebarLayout(
+        fluid = TRUE,
+        sidebarPanel(selectizeInput('e1', 'Select Team', choices =
+                                      NULL), width = 2),
+        mainPanel(
+          h2(textOutput("tname")),
+          tabsetPanel(
+            type = 'tabs',
+            #          tabPanel('Hitters',htmlOutput("TeamH")),
+            tabPanel('Hitters', dataTableOutput("TeamH")),
+            tabPanel('Pitchers', dataTableOutput("TeamP"))
+          )
         )
       )
-    )
-  ),
-  tabPanel("by Position",
-           sidebarLayout(
-             sidebarPanel(
-               selectizeInput(
-                 'e2', 'Select Position', choices=c('C','1B','2B','SS','3B','OF','SP','MR','CL')),
-               sliderInput("pd", "pDFL", 
-                           min=0, max=20, value=5)
-             ),
+    ),
+    tabPanel(
+      "Positional Surplus",
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput(
+            'e2',
+            'Select Position',
+            choices = c('C', '1B', '2B', 'SS', '3B', 'OF', 'SP', 'MR', 'CL')
+          ),
+          sliderInput(
+            "pd",
+            "pDFL",
+            min = 0,
+            max = 20,
+            value = 5
+          ),
+          width = 2
+        ),
+        mainPanel(dataTableOutput("tprofile"))
+      )
+    ),
+    tabPanel("Ranked Players",
+             sidebarLayout(
+               sidebarPanel(selectizeInput(
+                 'e3',
+                 'Select Position',
+                 choices = c('C', '1B', '2B', 'SS', '3B', 'OF', 'SP', 'MR', 'CL')
+               ), width = 1),
+               mainPanel(dataTableOutput("topPlayers"))
+             )),
+    tabPanel("LC Trends",
              mainPanel(
-               dataTableOutput("tprofile")
-             )
-           )
-  ),
-#   tabPanel("by Statistic",
-#            sidebarLayout(
-#              sidebarPanel(
-#                selectizeInput(
-#                  'e3', 'Select Statistic', choices=c('HR','RBI','R','SB','BA','W','K','ERA','HLD','SV'))
-#              ),
-#              mainPanel(
-#                h1('foo')
-#                #        tableOutput("Thitting")
-#              )
-#            )
-#   ),
-  tabPanel("Under Performers",
-           sidebarLayout(fluid=FALSE,
-                         sidebarPanel(
-                           sliderInput("hsc", "hotscore", 
-                                       min=0, max=20, value=8),
-                           sliderInput("upd", "pDFL", 
-                                       min=0, max=20, value=5),                         
-                           sliderInput("sal", "Salary", 
-                                       min=0, max=100, value=10)                           
-                         ),
-                         mainPanel(
-                           tabsetPanel(type='tabs',
-                                       tabPanel('Hitters',dataTableOutput("undH")),          
-                                       tabPanel('Pitchers',dataTableOutput("undP"))
-                           )
-                         )
-           )
+               tabsetPanel(
+                 type = 'tabs',
+                 tabPanel('Standings', plotOutput("g1")),
+                 tabPanel('Hitting', plotOutput("g2")),
+                 tabPanel('Pitching', plotOutput("g3"))
+               )
+             )),
+    tabPanel(
+      "LC Hitter Trends",
+      selectizeInput(
+        "choice",
+        "Pick Player",
+        choices = htrend$Player,
+        selected = NULL,
+        multiple = TRUE
+      ),
+      plotlyOutput("lcgraph")
+      
+    ),
+    tabPanel("Opportunities",
+             mainPanel(
+               h2("Injured or Sucky"),
+               dataTableOutput("problems")
+             ))
+    #   tabPanel("by Statistic",
+    #            sidebarLayout(
+    #              sidebarPanel(
+    #                selectizeInput(
+    #                  'e3', 'Select Statistic', choices=c('HR','RBI','R','SB','BA','W','K','ERA','HLD','SV'))
+    #              ),
+    #              mainPanel(
+    #                h1('foo')
+    #                #        tableOutput("Thitting")
+    #              )
+    #            )
+    #   ),
+    # tabPanel("Under Performers",
+    #          sidebarLayout(fluid=FALSE,
+    #                        sidebarPanel(
+    #                          sliderInput("hsc", "hotscore",
+    #                                      min=0, max=20, value=8),
+    #                          sliderInput("upd", "pDFL",
+    #                                      min=0, max=20, value=5),
+    #                          sliderInput("sal", "Salary",
+    #                                      min=0, max=100, value=10)
+    #                        ),
+    #                        mainPanel(
+    #                          tabsetPanel(type='tabs',
+    #                                      tabPanel('Hitters',dataTableOutput("undH")),
+    #                                      tabPanel('Pitchers',dataTableOutput("undP"))
+    #                          )
+    #                        )
+    #          )
+    # )
   )
-)
 )
